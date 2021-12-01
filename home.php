627448ffb0759477ca30722e7ce01a5881a6fe70
OsamaRagab4004 <45213572+OsamaRagab4004@users.noreply.github.com>
2021-12-01 22:11:43 +0100

    Update home.php

--- home.php
+++ home.php
@@ -2,48 +2,29 @@
             <div class="col-lg-6 col-md-12">
                 <img id="profImg" src="img.jpg" alt="">
             </div>
-            <div class="col-lg-6 col-md-12 TextContainer">
-                <h1 class="quoteText">“It's not what we do once in a
-                while that shapes our lives.
-                It's what we do consistently.”  </h1></div>
-            <div class="col-lg-12 col-md-12 marginBottom"></div>
+         
 
             <div class="col-lg-6 col-md-12">
                <div id="Home_text_container">
-                     <h1 class="text-align-center">Since When i learn Web developing ? <i class="fas fa-clock"></i></h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                     <h1 class="text-align-center">Wer bin ich ? <i class="fas fa-user-graduate lightred"></i></h1>
+                    <p>Ich studiere Informatik an der THM in Gießen,  bin 22 Jahre alt und liebe mein Studienfach sehr. Ich freue mich auf die Arbeit in diesem Bereich, um neben dem theoretischen Studium praktische Erfahrungen zu sammeln und die Grundlagen der Informatik zu beherrschen und praxisnah anzuwenden. Ich komme aus Ägypten und lebe seit zwei Jahren in Deutschland. Ein Jahr habe ich die Sprache gelernt und dann habe ich das Studuim angefangen.</p>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-12">
-                    <img class="imgHome" src="assets/img/whoami.png">
-            </div>
+            
             <div class="col-lg-12 col-md-12 marginBottom"></div>
            
+           
+                      
+
             <!--Answers for some Questions-->
             <div class="col-lg-12 col-md-12">
                 <div id="Home_text_container">
-                     <h1 class="text-align-center">Since When i learn Web developing ? <i class="fas fa-clock"></i></h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                     <h1 class="text-align-center">Lust auf Arbeit! <i class="fas fa-clock"></i></h1>
+                    <p>Ich möchte als Student in der Website-Entwicklung arbeiten, ich muss üben, mehr lernen und neue Herausforderungen erleben, 20 Stunden pro Woche kann ich arbeiten und mehr, wenn ich kein Studium habe. Es wird also eine sehr einzigartige Erfahrung sein, während des Studiums einen Job in meinem Bereich zu bekommen, 20 neue Stunden pro Woche, um neue Dinge zu lernen. Ich liebe es zu programmieren, daher ist es für mich wie ein Hobby.</p>
                 </div>
                 
             </div>
             <div class="col-lg-12 col-md-12 marginBottom"></div>
 
-            <div class="col-lg-12 col-md-12">
-              <div id="Home_text_container">
-                     <h1 class="text-align-center">Since When i learn Web developing ? <i class="fas fa-clock"></i></h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 marginBottom"></div>
-
-            <div class="col-lg-12 col-md-12">
-            <div id="Home_text_container">
-                     <h1 class="text-align-center">Since When i learn Web developing ? <i class="fas fa-clock"></i></h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ion ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumion ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 marginBottom"></div>
-
-
+            
         </div>
