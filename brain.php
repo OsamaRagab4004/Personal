044fdda0a577cc9ee2fa8e93dd65eb2733e750fa
OsamaRagab4004 <45213572+OsamaRagab4004@users.noreply.github.com>
2021-12-01 22:13:02 +0100

    Update brain.php

--- brain.php
+++ brain.php
@@ -2,8 +2,8 @@
 
 
 <div class="col-lg-12">
-    <h1> My Plans and Answers for some Questions. . . <i class="fas fa-brain lightred"></i></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore </p>
+    <h1> Meine Pläne und Antworten auf einige Fragen. . . <i class="fas fa-brain lightred"></i></h1>
+  
 </div>
 
 <div class="col-lg-12 marginBottom"></div>
@@ -16,8 +16,26 @@
       <button class="brainBlock" href="#" onclick="clickedBrain(this)">
         <div class="brain_container">
             <h1 class="head1 head">
-                <span><i class="fas fa-plus"></i></span> My Future learning path</h1>
-                <p class="brainText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <span><i class="fas fa-plus"></i></span> Was ist mein Plan für die nächsten sechs Monate? </h1>
+                <p class="brainText">Ich möchte Laravel weiter lernen und gut beherrschen, es hat mir sehr gut gefallen, vor allem weil es sehr organisiert ist und Zeit spart. Laravel ist die andere Seite von PHP, aber in einer weiter entwickelten Weise und mit größeren Möglichkeiten.<br><br>
+                Ich möchte auch meine Programmierfähigkeiten entwickeln und mich nach und nach schwierigeren Herausforderungen stellen, bei der Arbeit oder über Websites wie Codeforces.
+                <br> <br>
+                Ich möchte die Grundlagen von Algorithmen und Datenstruktur beherrschen, die ich bereits an der Uni studiere und selbst ein wunderbares Buch namens grokking-algorithms lese.
+                <br> <br>
+                Beherrschen der SASS-Technik, wie sie in der Organisation der Arbeit an großen Projekten sehr wichtig ist.
+                <br> <br>
+                Ich lerne neue Techniken wie React.js oder die erforderliche Technik, je nachdem, was bei der Arbeit erforderlich ist. Oder wenn ich die freie Wahl hätte, würde ich auch lernen, was meinen Job professioneller und einfacher macht.
+                <br> <br>
+                Adobe XD lernen.
+                <br><br>
+                Umgang mit dem APIS
+                <br><br>
+                Ich möchte auch lernen, wie man Websites auf fortgeschrittene Weise sichert. Ich habe ein kleines Buch über Cyberangriffe gelesen, aber es reicht nicht, ich muss vielleicht mehr darüber erfahren.
+
+
+
+            </p>
+                
         </div>
         
 
@@ -30,12 +48,17 @@
 
 
 
+   
+
+
+
+
     <div class="col-lg-12">
       <button class="brainBlock" href="#" onclick="clickedBrain(this)">
         <div class="brain_container">
             <h1 class="head1 head">
-                <span><i class="fas fa-plus"></i></span> My Future learning path</h1>
-                <p class="brainText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <span><i class="fas fa-plus"></i></span> Wie viele Stunden möchte ich pro Woche arbeiten?</h1>
+                <p class="brainText">Zwanzig Stunden an Studientagen und in den Ferien möchte ich mehr arbeiten.</p>
         </div>
         
 
@@ -47,21 +70,21 @@
     <div class="col-lg-12 marginBottom-small"></div>
 
 
-
-    <div class="col-lg-12">
-      <button class="brainBlock" href="#" onclick="clickedBrain(this)">
-        <div class="brain_container">
-            <h1 class="head1 head">
-                <span><i class="fas fa-plus"></i></span> My Future learning path</h1>
-                <p class="brainText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        
-
-        </button>
-      
+<div class="col-lg-12">
+  <button class="brainBlock" href="#" onclick="clickedBrain(this)">
+    <div class="brain_container">
+        <h1 class="head1 head">
+            <span><i class="fas fa-plus"></i></span> Fullstack-Webentwickler</h1>
+            <p class="brainText">Ich kann als Fullstack-Webentwickler arbeiten, aber ich bevorzuge es, auch mehr mit der Backend-Seite zu arbeiten, daher wird es großartig sein, sowohl mit Design als auch mit Programmierung zu arbeiten..</p>
     </div>
+    
+
+    </button>
+  
+</div>
 
 
+    
     <div class="col-lg-12 marginBottom"></div>
   
 
